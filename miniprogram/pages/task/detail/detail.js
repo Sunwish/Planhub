90c21172aa34c22a47d19425d8c29a26bbc11b7f
Sunwish <770168287@qq.com>
2020-04-29 20:53:47 +0800

    停用createSub页面，将添加子任务功能嵌入detail页的pop层
    detail页添加子任务处理人详情菜单按钮，具体功能待实现

--- miniprogram/pages/task/detail/detail.js
+++ miniprogram/pages/task/detail/detail.js
@@ -14,6 +14,24 @@ Page({
     loading: true,
     tid: '',
     checked: false,
+    showSubTaskHandlerDetialMenu: false,
+    subTaskHandlerDetialTitle: '',
+    showAddSubTaskPop: false,
+    "activeTab": 0,
+    "date": '',
+    "show": false,
+    "radio": '1',
+    "description": '',
+    "name": '',
+    subTaskHandlerDetialMenuActions: [
+      {
+        name: '添加处理人'
+      },
+      {
+        name: '邀请好友加入处理',
+        openType: 'share'
+      }
+    ],
     steps: [
       {
         text: '未开始',
@@ -82,8 +100,38 @@ Page({
   },
 
   addSubTask: function (){
+    this.setData({
+      showAddSubTaskPop: true
+    })
+    return
     wx.navigateTo({
       url: '/pages/task/createSub/createSub?taskId=' + this.data.tid,
+    })
+  },
+
+  subTaskHandlerDetial: function(e){
+    // console.log(e)
+    this.setData({
+      showSubTaskHandlerDetialMenu: true,
+      subTaskHandlerDetialTitle: e.currentTarget.dataset.subtaskname
+    })
+    // console.log(e.currentTarget.id)
+  },
+
+  onSubTaskHandlerDetialMenuSelect(event) {
+    wx.showToast({
+      icon: 'none',
+      title: event.detail.name
+    })
+  },
+
+  onSubTaskHandlerDetialMenuClose() {
+    this.setData({ showSubTaskHandlerDetialMenu: false });
+  },
+
+  onAddSubTaskPopClose: function(){
+    this.setData({
+      showAddSubTaskPop: false
     })
   },
 
@@ -99,6 +147,135 @@ Page({
     //var tid = this.data.tid;
     //this.onShareAppMessage(tid);
   //},
+
+  descriptionChange(event) {
+    this.setData({
+      description: event.detail
+    })
+  },
+
+  nameChange(event) {
+    this.setData({
+      name: event.detail
+    })
+  },
+
+  /**
+   * 日历展示 */
+  onDisplay() {
+    this.setData({
+      show: true
+    });
+  },
+  onClose() {
+    this.setData({
+      show: false
+    });
+  },
+  onConfirm(event) {
+    const [start, end] = event.detail;
+    this.setData({
+      show: false,
+      date: `${this.formatDate(start)} - ${this.formatDate(end)}`
+    });
+  },
+  /**
+   * 返回时间 */
+  formatDate(date) {
+    date = new Date(date);
+    return `${date.getMonth() + 1}/${date.getDate()}`;
+  },
+  /**
+   * 选择框的提醒 */
+  onRadioChange(event) {
+    this.setData({
+      radio: event.detail
+    });
+  },
+
+  onClick(event) {
+    const {
+      name
+    } = event.currentTarget.dataset;
+    this.setData({
+      radio: name
+    });
+  },
+
+  onCreateSubTask: function (event) {
+    var taskName = this.data.name;
+    var deadline = this.data.date;
+    var taskDescription = this.data.description;
+    var _parentId = this.data.tid;
+    var _participantsId = [];
+    var _handlersId = [];
+    var status = 0;
+    var creationTime = Date.now();
+    var priority = 0;
+    var subTasksId = [];
+
+    var taskAttr = {
+      "taskName": taskName,
+      "taskDescription": taskDescription,
+      "_parentId": _parentId,
+      "_participantsId": _participantsId,
+      "_handlersId": _handlersId,
+      "status": status,
+      "creationTime": creationTime,
+      "deadline": deadline,
+      "priority": priority,
+      "subTasksId": subTasksId
+    };
+    this._onCreateSubTask(taskAttr);
+  },
+
+  _onCreateSubTask: function (taskAttr) {
+    console.log(taskAttr)
+    const db = wx.cloud.database();
+    const _ = db.command;
+    db.collection('tasks').add({
+      data: {
+        taskName: taskAttr["taskName"],
+        taskDescription: taskAttr["taskDescription"],
+        _parentId: taskAttr["_parentId"],
+        _participantsId: taskAttr["_participantsId"],
+        _handlersId: taskAttr["_handlersId"],
+        status: taskAttr["status"],
+        creationTime: taskAttr["creationTime"],
+        deadline: taskAttr["deadline"],
+        priority: taskAttr["priority"],
+        subTasksId: taskAttr["subTasksId"]
+      },
+      success: res => { /* 子任务创建成功，下面将子任务ID添加到父任务的子任务ID数组中 */
+        db.collection('tasks').doc(taskAttr._parentId).update({
+          data: {
+            subTasksId: _.push(res._id)
+          },
+          success: res2 => {
+            wx.showToast({
+              title: '添加子任务成功',
+            })
+            this.onAddSubTaskPopClose();
+            this.onShow();
+          },
+          fail: err2 => {
+            /* 子任务创建成功但是绑定失败，删除子任务 */
+            db.collection('todos').doc('res._id').remove({})
+            wx.showToast({
+              title: '子任务添加失败，请尝重新添加',
+            })
+          }
+        });
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '创建任务失败'
+        })
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
