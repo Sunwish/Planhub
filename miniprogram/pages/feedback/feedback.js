644fe47295f7b5c84ae4781ca0931c70737e7dd0
urafrik <51160007+urafrik@users.noreply.github.com>
2020-05-12 20:31:20 +0800

    重做了feedback

--- miniprogram/pages/feedback/feedback.js
+++ miniprogram/pages/feedback/feedback.js
@@ -1,75 +1,45 @@
-// pages/feedback/feedback.js
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-     
+    loading: false,
+    contact: '',
+    contant: ''
   },
-  submitfeedback: function(){
-    wx.showToast({
-      title: '提交成功',
-    })
-    setTimeout(function () {
-      wx.navigateBack({})
-    }, 1000)
+ 
+  formSubmit: function (e) {
+    let _that = this;
+    let content = e.detail.value.opinion;
+    let contact = e.detail.value.contact;
+    if (content == "" && contact == "") {
+      wx.showModal({
+        title: '提示',
+        content: '什么都没写哦！',
+      })
+      return false
+    }
+    if (content == "" && contact != "") {
+      wx.showModal({
+        title: '提示',
+        content: '写一点什么吧！',
+      })
+      return false
+    }
+    if (content != "" && contact == "") {
+      wx.showModal({
+        title: '提示',
+        content: '还没留下联系方式呢!',
+      })
+      return false
+    }else {
+      wx.showToast({
+        title: '提交成功',
+      })
+      setTimeout(function () {
+        wx.navigateBack({})
+      }, 1000)
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
