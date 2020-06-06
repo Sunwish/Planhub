6e27037438950c8748ce7955069e968a914c9586
Plane guan <49422318+Planeguan@users.noreply.github.com>
2020-06-07 00:41:54 +0800

    点击图片可以预览拉！！还可以左右滑动哟

--- miniprogram/pages/task/detail/detail.js
+++ miniprogram/pages/task/detail/detail.js
@@ -947,5 +947,14 @@ Page({
         console.log('成功', res);
       }
     }
-  }
+  },
+  clickImg: function(e){
+  var src = e.currentTarget.dataset.src;//获取data-src
+  var imgList = e.currentTarget.dataset.list;//获取data-list
+  //图片预览
+  wx.previewImage({
+  current: src, // 当前显示图片的http链接
+  urls: imgList // 需要预览的图片http链接列表
+  })
+  },
 })
